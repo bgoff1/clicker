5086baa429b0e1df7fe2fae037a030a4504acbe1
Judah Goff <35714383+bgoff1@users.noreply.github.com>
2019-12-05 11:55:25 -0600

    Add themes (#3)
    * Add themes and button-click component
    
    * Add bulma

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ButtonClickComponent } from '@components/button-click/button-click.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: '', component: ButtonClickComponent }];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { useHash: true })],
+  imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
