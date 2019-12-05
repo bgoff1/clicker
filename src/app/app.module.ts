5774bc7776b204ec4ee1290712e6e09ed4c42d53
Judah Goff <bgoff1@harding.edu>
2019-12-04 22:17:48 -0600

    Add themes and button-click component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,18 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ButtonComponent } from './components/button/button.component';
+import { ButtonClickComponent } from './components/button-click/button-click.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    ButtonComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, ButtonClickComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/components/button-click/button-click.component.spec.ts
@@ -0,0 +1,13 @@
+import { ButtonClickComponent } from './button-click.component';
+
+describe('ButtonClickComponent', () => {
+  let component: ButtonClickComponent;
+
+  beforeEach(() => {
+    component = new ButtonClickComponent();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
